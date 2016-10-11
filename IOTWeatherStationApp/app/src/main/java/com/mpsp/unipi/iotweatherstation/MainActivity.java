c15b55f3ba5785d005aac6b97df2e56ff55b0073
George Emmanouil <geo.emmnl@gmail.com>
2016-10-11 22:48:02 +0300

    About page is complete

--- IOTWeatherStationApp/app/src/main/java/com/mpsp/unipi/iotweatherstation/MainActivity.java
+++ IOTWeatherStationApp/app/src/main/java/com/mpsp/unipi/iotweatherstation/MainActivity.java
@@ -1,7 +1,13 @@
 package com.mpsp.unipi.iotweatherstation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dropdownmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.settings:
+
+                return true;
+            case R.id.about:
+                about();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void about() {
+        Intent i = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(i);
     }
 }

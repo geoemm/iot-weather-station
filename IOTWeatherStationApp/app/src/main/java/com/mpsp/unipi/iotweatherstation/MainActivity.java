30cc9029b4ebcb8c063227e1acf3d6c6fccfa3a5
hybridkozo <g.kozompolis@gmail.com>
2016-11-08 01:26:36 +0200

    Add Firebase Analytics
    When a user access via "About" menu item then an event triggered. You
    can watch the events at firebase dashboard.

--- IOTWeatherStationApp/app/src/main/java/com/mpsp/unipi/iotweatherstation/MainActivity.java
+++ IOTWeatherStationApp/app/src/main/java/com/mpsp/unipi/iotweatherstation/MainActivity.java
@@ -9,12 +9,21 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+
+    // [START declare_analytics]
+    private FirebaseAnalytics mFirebaseAnalytics;
+    // [END declare_analytics]
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
 
     }
@@ -34,6 +43,14 @@ public class MainActivity extends AppCompatActivity {
 
                 return true;
             case R.id.about:
+                // [START image_view_event]
+                Bundle bundle = new Bundle();
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, String.valueOf(item.getItemId()));
+                bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, String.valueOf(item.getTitle()));
+                bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "button");
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
+                // [END image_view_event]
+
                 about();
                 return true;
             default:
@@ -43,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void about() {
         Intent i = new Intent(MainActivity.this, AboutActivity.class);
+
+
         startActivity(i);
     }
 }

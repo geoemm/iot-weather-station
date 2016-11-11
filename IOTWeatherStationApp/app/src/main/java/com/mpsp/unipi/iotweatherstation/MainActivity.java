d407e0bacbe4d0da2bc0678be0ebfb3dd884d9d1
hybridkozo <g.kozompolis@gmail.com>
2016-11-11 20:58:18 +0200

    Add Notification receiver

--- IOTWeatherStationApp/app/src/main/java/com/mpsp/unipi/iotweatherstation/MainActivity.java
+++ IOTWeatherStationApp/app/src/main/java/com/mpsp/unipi/iotweatherstation/MainActivity.java
@@ -8,8 +8,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.mpsp.unipi.iotweatherstation.firebase.MyFirebaseInstanceIDService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         // Obtain the FirebaseAnalytics instance.
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
---
+++ IOTWeatherStationApp/app/src/main/java/com/mpsp/unipi/iotweatherstation/firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,63 @@
+package com.mpsp.unipi.iotweatherstation.firebase;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.mpsp.unipi.iotweatherstation.MainActivity;
+import com.mpsp.unipi.iotweatherstation.R;
+
+/**
+ * Created by User on 10/11/2016.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        //Displaying data in log
+        //It is optional
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+        //Calling method to generate notification
+        sendNotification(remoteMessage.getNotification().getBody());
+    }
+
+    //This method to generate push notification
+    private void sendNotification(String messageBody) {
+
+        //MainActivity Intent Registration
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        //Take Notification Sound
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        //Generate the Notification
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Firebase Notification")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //Create Push Notification
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}

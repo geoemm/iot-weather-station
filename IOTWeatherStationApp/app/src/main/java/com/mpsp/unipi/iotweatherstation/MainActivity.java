ceb9b906a53c8264c6519e99e0021ec8f1a41025
Georgios Emmanouil <geo.emmnl@gmail.com>
2016-11-11 21:33:32 +0200

    test

--- IOTWeatherStationApp/app/src/main/java/com/mpsp/unipi/iotweatherstation/MainActivity.java
+++ IOTWeatherStationApp/app/src/main/java/com/mpsp/unipi/iotweatherstation/MainActivity.java
@@ -3,11 +3,20 @@ package com.mpsp.unipi.iotweatherstation;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.PopupMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +25,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        final Button button = (Button) findViewById(R.id.syncBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                bringData();
+            }
+        });
     }
+
+    private void bringData() {
+        final TextView mTextView = (TextView) findViewById(R.id.textView);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.4.1:8080";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,10));
+                        //mTextView.setText("Response is: " + ((response.length()>499)?response.substring(0,500):response));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

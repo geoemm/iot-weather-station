a6dcee6358201dce0fe848f357d2cce9f6c99faf
Georgios Emmanouil <geo.emmnl@gmail.com>
2016-11-12 10:25:50 +0200

    About page is complete

--- IOTWeatherStationApp/app/src/main/java/com/mpsp/unipi/iotweatherstation/AboutActivity.java
+++ IOTWeatherStationApp/app/src/main/java/com/mpsp/unipi/iotweatherstation/AboutActivity.java
@@ -1,11 +1,14 @@
 package com.mpsp.unipi.iotweatherstation;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.util.Linkify;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -16,7 +19,24 @@ public class AboutActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        TextView geoemailView = (TextView) findViewById(R.id.textVGEOEMAIL);
+        geoemailView.setText(R.string.geoEmail);
+        Linkify.addLinks(geoemailView, Linkify.ALL);
 
+        TextView kozoemailView = (TextView) findViewById(R.id.textVKOZOEMAIL);
+        kozoemailView.setText(R.string.kozoEmail);
+        Linkify.addLinks(kozoemailView, Linkify.ALL);
+
+        ImageView img = (ImageView)findViewById(R.id.imageView);
+        img.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.cs.unipi.gr/index.php?lang=en"));
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # IOT Weather Station
-A simple weather station based on ESP8266, added android connectivity and online data storage using Firebase (or MongoDB).
+A simple weather station based on ESP8266, added android connectivity and online data storage using Firebase.
 
-Temperature, humidity and light sensor data are transfered throught an ESP8266 module to an android application and end up in an online database.  
+Temperature, humidity and light sensor measurements are transfered throught an ESP8266 module to an android application and end up in an online database.
